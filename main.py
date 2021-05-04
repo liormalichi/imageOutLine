7228e09141fed73f84e22bbaef93bd7de8012329
Lior Malichi <63522056+liormalichi@users.noreply.github.com>
2021-05-04 11:56:42 +0300

    Update main.py
    exetend epalantion of the code

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 """
-here e going to take a photo and after running the code the outlinne of the object(s) 
+here we going to take a photo and after running the code the outlinne of the object(s) 
 in the photo will apprear in a new image!
+
+essentelly the code checks near every pixel if at least 4 of his "neighbors pixels" are differ from it by "blckfactor" amount, if so its probably a outline.
 """
 def change_image(im):
     im2 = im
